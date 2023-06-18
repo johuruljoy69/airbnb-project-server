3202c8b4a5dab3418575f141f73303ae9c8c965f
johirjoy <joy.sd69@gmail.com>
2023-06-19 00:09:19 +0600

    After confirm booking send a verify email api setup

--- index.js
+++ index.js
@@ -36,6 +36,41 @@ const verifyJWT = (req, res, next) => {
         req.decoded = decoded
         next()
     })
+};
+
+// Send Email
+const sendMail = (emailData, emailAddress) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS,
+        },
+    })
+
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log('Server is ready to take our messages')
+        }
+    })
+
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: emailAddress,
+        subject: emailData?.subject,
+        html: `<p>${emailData?.message}</p>`,
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log('Email sent: ' + info.response)
+        }
+    })
 }
 
 
